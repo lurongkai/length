a917f1a26e4789356c16bdcac8edb9e7407781e9
ChanceDoor <cd19900419@hotmail.com>
2013-08-01 12:34:48 -0400

    better

--- standard.rb
+++ standard.rb
@@ -11,12 +11,11 @@ class Worker
       if line == "\n"
         @p = @p + 1
       else
+        factor= line.split(' ')
         case @p
         when 0
-          factor= line.split(' ')
           @rule[factor[1]] = factor[3]
         when 1
-          factor = line.split(" ")
           if (line.include? '+') || (line.include? '-')
             equation = []
             factor.each_with_index do |array,index|
@@ -27,33 +26,28 @@ class Worker
                 equation.push( array.to_f * @rule[unit].to_f)
               end
             end
-             answer = eval(equation.join)
+            answer = eval(equation.join)
           else
             unit = singularize(factor[1])
             answer = factor[0].to_f * @rule[unit].to_f
           end
-          answer = format("%.2f",answer)
-          @result.push(answer.to_s+" m\n")
+          @result.push(format("%.2f",answer).to_s+" m\n")
         end
       end
     end 
-  @result
+    @result
   end
 
   def singularize(s)
+    dic = {"s$" => '',"es$" => '',"ee" => 'oo'}
     return s if @rule[s]
-    z = s.gsub(/s$/,'')
-    return z if @rule[z]
-    z = s.gsub(/es$/,'') 
-    return z if @rule[z]
-    z = s.gsub(/ee/,'oo')
-    return z if @rule[z]
+    dic.each do |k,v|
+      z = s.gsub(/#{k}/,v)
+      return z if @rule[z]
+    end
     s  
   end
-
 end
 
-new_work = Worker.new(File.open("input.txt"))
-@rs = new_work.run
-file = File.new("output.txt","w")
-file.puts(@rs)
+@rs = Worker.new(File.open("input.txt")).run
+File.new("output.txt","w").puts(@rs)
